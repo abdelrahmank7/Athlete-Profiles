fd2b612ef4fe4eaa5ed52fab6efce67f6508f842
abdelrahmank7 <abdelrahmankhaled7@icloud.com>
2025-02-25 17:14:12 +0200

    chnage some css

--- public/js/athlete.js
+++ public/js/athlete.js
@@ -12,12 +12,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   await initializeAthletePage(athleteId);
 
-  // Add save button click event listener
-  const saveButton = document.getElementById("import-button");
-  if (saveButton) {
-    saveButton.addEventListener("click", async () => {
-      console.log("Save button clicked");
-      // We will implement the save functionality here step by step
-    });
-  }
+  // // Add save button click event listener
+  // const saveButton = document.getElementById("save-button");
+  // if (saveButton) {
+  //   saveButton.addEventListener("click", async () => {
+  //     console.log("Save button clicked");
+  //     // We will implement the save functionality here step by step
+  //   });
+  // }
 });
